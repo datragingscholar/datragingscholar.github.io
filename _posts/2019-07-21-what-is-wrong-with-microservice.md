c98041aea1c888a420e5244e30fc9eb6eef652cf
ragingscholar <ragingscholar@protonmail.com>
2019-07-21 16:43:58 +0800

    testing out configs

---
+++ _posts/2019-07-21-what-is-wrong-with-microservice.md
@@ -0,0 +1,9 @@
+---
+title:  "What is wrong with microservice"
+---
+
+# Welcome
+
+**Hello world**, this is my first Jekyll blog post.
+
+I hope you like it!
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "What is wrong with microservice",
+        "excerpt":"Welcome   Hello world, this is my first Jekyll blog post.   I hope you like it!  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/what-is-wrong-with-microservice/",
+        "teaser":null}]
